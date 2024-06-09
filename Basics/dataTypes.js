747c84c3572585d97fb0cfa51ee74a90a1795b8c
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-06-09 02:55:16 +0000

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+
+let name = "Achyut"   //string data type
+let age = 20          //number data type
+let isLoggedIn = true //boolean data type
+
+//null => standalone value (representation of empty value)
+//undefined => not assigning any value
+//symbol => uniqueness of component (used mostly in react and figma files)
+
+//object => In JavaScript, an object is a collection of key-value pairs, where keys are strings (or symbols) and values can be any data type, including other objects. 
+//objects are used to store and organize data and can represent real-world entities.
+//object is a complex data type used to store collections of data and more complex entities. 
+//objects in JavaScript are one of the core components, allowing the creation of structured data and enabling object-oriented programming.
+
+console.log(typeof "Achyut");  //tells the data type of a value 
+console.log(typeof age);       //tells the data type of a variable
+
+//NOTE: the data type of null is object
+console.log(typeof null);
