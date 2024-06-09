2469e653f5055f0f4c74c491cc489c6c7d5f29e6
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-06-09 06:42:49 +0000

    comparison between data types

---
+++ Basics/comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+console.log("2" > 1);
+console.log("02" > 1);
+//automatically JS converts the "2" or "02" into a number and not a string
+
+console.log("2" == 2); //here again JS converts "2" to number and then compares 
+
+console.log("2" === 2); //now JS will also check the data types and then decides whether it should compare
