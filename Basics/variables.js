c7fe529dae71dfa7a93bf4018cc42fabeca28bb6
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-06-09 02:54:57 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "akd@gmail.com";
+var accountPassword = "123456";
+accountCity = "Jorhat";
+let accountState;
+
+//let, const, var: assigns the value inside the memory
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+//for declaring constants and varibles mainly const and let is used
+//prefer not to use var becasue of the issue in block scope and  functional scope
+
+console.log(accountState);
+
+//we can declare a variable even without assigning any value
+//JS considers this as undefined variable
