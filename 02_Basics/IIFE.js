22d3b28afc73f91734bb75198ba3e8d346ff31bb
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-06-12 10:20:16 +0000

    iife

---
+++ 02_Basics/IIFE.js
@@ -0,0 +1,7 @@
+//Immediately Invoked Function Expression (IIFE)
+//(function declaration)(function call)
+//it is used when the function that is declared should be consoled immediately
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+})()
