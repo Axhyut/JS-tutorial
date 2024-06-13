871d84a46b782fd8d5e00b8c7027a0faaa1648b3
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-06-13 01:03:18 +0000

    if statement

---
+++ 03_control_flow/one.js
@@ -0,0 +1,14 @@
+//if
+
+// if (condition) {
+    
+// }
+
+//if the condition is true then the if function will execute 
+//if the condition is false then the code inside the scope will not get executed
+
+const score = 200
+if(score > 100){
+    const power = "fly"
+    console.log(`User power: ${power}`);
+}
